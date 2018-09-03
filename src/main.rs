767f431e187305204cbd92d8453f2d912d6cc066
Seiya Tokui <beam.web@gmail.com>
2018-09-04 00:50:10 +0900

    Emit IR

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+extern crate llvm_sys;
+
 use std::borrow::Cow;
 use std::collections::HashMap;
+use std::ffi::{CStr, CString};
 use std::str::Chars;
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use std::iter::Peekable;
 
+use llvm_sys::prelude::*;
+
+fn cstr(s: &str) -> Box<CStr> {
+    CString::new(s).unwrap().into_boxed_c_str()
+}
+
 type ParseResult<T> = Result<T, Cow<'static, str>>;
+type CompileResult<T> = Result<T, Cow<'static, str>>;
 
 #[derive(Debug, PartialEq)]
 enum Token {
@@ -45,21 +55,21 @@ impl<'a> Lexer<'a> {
 
         // skip any whitespace
         loop {
-            match self.src.peek() {
+            match self.peekc() {
                 Some(&'#') => {  // comment
-                    while let Some(c) = self.src.next() {
+                    while let Some(c) = self.getc() {
                         if c == '\r' || c == '\n' {
                             break;
                         }
                     }
                 },
-                Some(&c) if c.is_whitespace() => { self.src.next(); },
+                Some(&c) if c.is_whitespace() => { self.getc(); },
                 Some(_) => break,
                 None => return Ok(Token::Eof),
             }
         }
 
-        let c = *self.src.peek().unwrap();
+        let c = *self.peekc().unwrap();
 
         // identifier: [a-zA-Z][a-zA-Z0-9]*
         if c.is_ascii_alphabetic() {
@@ -80,7 +90,7 @@ impl<'a> Lexer<'a> {
         }
 
         // operator
-        match self.src.next() {
+        match self.getc() {
             Some(c) => Ok(Token::Op(c)),
             None => Ok(Token::Eof),
         }
@@ -99,39 +109,41 @@ impl<'a> Lexer<'a> {
     // Reads from src while pred returns true.
     // It assumes that pred holds for the first character (thus skipping test with pred).
     fn read_while(&mut self, pred: impl Fn(char) -> bool) -> String {
-        let mut s = self.src.next().unwrap().to_string();
-        while let Some(&c) = self.src.peek() {
+        let mut s = self.getc().unwrap().to_string();
+        while let Some(&c) = self.peekc() {
             if !pred(c) {
                 break;
             }
-            s.push(self.src.next().unwrap());
+            s.push(self.getc().unwrap());
         }
         s
     }
-}
 
-#[derive(Debug)]
-struct BinaryExprAst {
-    op: char,
-    lhs: Box<ExprAst>,
-    rhs: Box<ExprAst>,
-}
+    fn peekc(&mut self) -> Option<&char> {
+        self.src.peek()
+    }
 
-impl BinaryExprAst {
-    fn new(op: char, lhs: ExprAst, rhs: ExprAst) -> BinaryExprAst {
-        BinaryExprAst { op, lhs: Box::new(lhs), rhs: Box::new(rhs) }
+    fn getc(&mut self) -> Option<char> {
+        self.src.next()
     }
 }
 
-#[derive(Debug)]
-struct CallExprAst {
-    callee: String,
-    args: Vec<ExprAst>,
+struct LLVM {
+    ctx: LLVMContextRef,
+    builder: LLVMBuilderRef,
+    module: LLVMModuleRef,
+    named_values: HashMap<String, LLVMValueRef>,
 }
 
-impl CallExprAst {
-    fn new(callee: String, args: Vec<ExprAst>) -> CallExprAst {
-        CallExprAst { callee, args }
+impl LLVM {
+    fn new() -> LLVM {
+        let name = cstr("my cool jit");
+        unsafe {
+            let ctx = llvm_sys::core::LLVMContextCreate();
+            let builder = llvm_sys::core::LLVMCreateBuilder();
+            let module = llvm_sys::core::LLVMModuleCreateWithNameInContext(name.as_ptr(), ctx);
+            LLVM { ctx, builder, module, named_values: HashMap::new() }
+        }
     }
 }
 
@@ -139,8 +151,74 @@ impl CallExprAst {
 enum ExprAst {
     Number(f64),
     Variable(String),
-    Binary(BinaryExprAst),
-    Call(CallExprAst),
+    Binary { op: char, lhs: Box<ExprAst>, rhs: Box<ExprAst> },
+    Call { callee: String, args: Vec<ExprAst> },
+}
+
+impl ExprAst {
+    fn codegen(&self, llvm: &mut LLVM) -> CompileResult<LLVMValueRef> {
+        match self {
+            ExprAst::Number(value) => unsafe {
+                let ty = llvm_sys::core::LLVMDoubleTypeInContext(llvm.ctx);
+                Ok(llvm_sys::core::LLVMConstReal(ty, *value))
+            },
+            ExprAst::Variable(name) => {
+                match llvm.named_values.get(name) {
+                    Some(value) => Ok(*value),
+                    None => Err(Cow::from("unknown variable name")),
+                }
+            },
+            ExprAst::Binary { op, lhs, rhs } => {
+                let lhs = lhs.codegen(llvm)?;
+                let rhs = rhs.codegen(llvm)?;
+                match op {
+                    '+' => unsafe {
+                        let name = cstr("addtmp");
+                        Ok(llvm_sys::core::LLVMBuildFAdd(llvm.builder, lhs, rhs, name.as_ptr()))
+                    },
+                    '-' => unsafe {
+                        let name = cstr("subtmp");
+                        Ok(llvm_sys::core::LLVMBuildFSub(llvm.builder, lhs, rhs, name.as_ptr()))
+                    },
+                    '*' => unsafe {
+                        let name = cstr("multmp");
+                        Ok(llvm_sys::core::LLVMBuildFMul(llvm.builder, lhs, rhs, name.as_ptr()))
+                    },
+                    '<' => unsafe {
+                        let name_cmp = cstr("cmptmp");
+                        let name_bool = cstr("booltmp");
+
+                        let b = llvm_sys::core::LLVMBuildFCmp(
+                            llvm.builder, llvm_sys::LLVMRealPredicate::LLVMRealULT, lhs, rhs, name_cmp.as_ptr());
+                        let ty = llvm_sys::core::LLVMDoubleTypeInContext(llvm.ctx);
+                        Ok(llvm_sys::core::LLVMBuildUIToFP(llvm.builder, b, ty, name_bool.as_ptr()))
+                    },
+                    _ => Err(Cow::from("invalid binary operator")),
+                }
+            },
+            ExprAst::Call { callee, args } => {
+                let name = cstr(&callee);
+                let callee_f = unsafe { llvm_sys::core::LLVMGetNamedFunction(llvm.module, name.as_ptr()) };
+
+                let mut args_v = vec![];
+                for arg in args {
+                    let arg_v = arg.codegen(llvm)?;
+                    if arg_v.is_null() {
+                        return Err(Cow::from("invalid function argument"));
+                    }
+                    args_v.push(arg_v);
+                }
+                let n_args = unsafe { llvm_sys::core::LLVMCountParams(callee_f) };
+                if n_args as usize != args_v.len() {
+                    return Err(Cow::from(format!("invalid number of arguments: {} vs {}", n_args, args_v.len())));
+                }
+                let name = cstr("calltmp");
+                unsafe {
+                    Ok(llvm_sys::core::LLVMBuildCall(llvm.builder, callee_f, args_v.as_mut_ptr(), n_args, name.as_ptr()))
+                }
+            },
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -153,6 +231,24 @@ impl PrototypeAst {
     fn new(name: String, args: Vec<String>) -> PrototypeAst {
         PrototypeAst { name, args }
     }
+
+    fn codegen(&self, llvm: &mut LLVM) -> LLVMValueRef {
+        let n_args = self.args.len();
+        let double = unsafe { llvm_sys::core::LLVMDoubleTypeInContext(llvm.ctx) };
+        let mut doubles = vec![double; n_args];
+        let ft = unsafe { llvm_sys::core::LLVMFunctionType(double, doubles.as_mut_ptr(), n_args as u32, 0) };
+        let name = cstr(&self.name);
+
+        // TODO(beam2d): linkage?
+        let f = unsafe { llvm_sys::core::LLVMAddFunction(llvm.module, name.as_ptr(), ft) };
+
+        for i in 0..n_args {
+            let param = unsafe { llvm_sys::core::LLVMGetParam(f, i as u32) };
+            let name = cstr(&self.args[i]);
+            unsafe { llvm_sys::core::LLVMSetValueName(param, name.as_ptr()) };
+        }
+        f
+    }
 }
 
 #[derive(Debug)]
@@ -164,6 +260,51 @@ struct FunctionAst {
 impl FunctionAst {
     fn new(proto: PrototypeAst, body: ExprAst) -> FunctionAst {
         FunctionAst { proto, body }
+    }
+
+    fn codegen(&self, llvm: &mut LLVM) -> CompileResult<LLVMValueRef> {
+        let name = cstr(&self.proto.name);
+        let mut f = unsafe { llvm_sys::core::LLVMGetNamedFunction(llvm.module, name.as_ptr()) };
+        if f.is_null() {
+            f = self.proto.codegen(llvm);
+            if f.is_null() {
+                return Err(Cow::from("function cannot be created"));
+            }
+        }
+        let empty = unsafe { llvm_sys::core::LLVMCountBasicBlocks(f) } == 0;
+        if !empty {
+            return Err(Cow::from("function cannot be redefined"));
+        }
+
+        let name = cstr("entry");
+        unsafe {
+            let bb = llvm_sys::core::LLVMAppendBasicBlock(f, name.as_ptr());
+            llvm_sys::core::LLVMPositionBuilderAtEnd(llvm.builder, bb);
+        }
+
+        llvm.named_values.clear();
+        let n_args = self.proto.args.len();
+        for i in 0..n_args {
+            let param = unsafe { llvm_sys::core::LLVMGetParam(f, i as u32) };
+            llvm.named_values.insert(self.proto.args[i].clone(), param);
+        }
+
+        match self.body.codegen(llvm) {
+            Ok(ret_val) => unsafe {
+                llvm_sys::core::LLVMBuildRet(llvm.builder, ret_val);
+                match llvm_sys::analysis::LLVMVerifyFunction(f, llvm_sys::analysis::LLVMVerifierFailureAction::LLVMPrintMessageAction) {
+                    0 => Ok(f),
+                    _ => {
+                        llvm_sys::core::LLVMDeleteFunction(f);
+                        Err(Cow::from("invalid function"))
+                    },
+                }
+            },
+            Err(e) => unsafe {
+                llvm_sys::core::LLVMDeleteFunction(f);
+                Err(e)
+            },
+        }
     }
 }
 
@@ -184,7 +325,7 @@ impl<'a> Parser<'a> {
 
     // ident_expr
     //     ::= ident
-    //     ::= ident '(' expr ')'
+    //     ::= ident '(' expr* ')'
     fn parse_ident_expr(&mut self, name: String) -> ParseResult<ExprAst> {
         match self.lexer.peektok()? {
             Token::Op('(') => { self.lexer.gettok().unwrap(); },
@@ -192,17 +333,17 @@ impl<'a> Parser<'a> {
         }
 
         let mut args = vec![];
-        while *self.lexer.peektok()? != Token::Op('(') {
+        while *self.lexer.peektok()? != Token::Op(')') {
             let arg = self.parse_expr()?;
+            args.push(arg);
             if let Token::Op(')') = self.lexer.peektok()? {
                 break;
             }
             self.consume_tok(&Token::Op(','))?;
-            args.push(arg);
         }
         self.lexer.gettok().unwrap();  // Op(')')
 
-        Ok(ExprAst::Call(CallExprAst::new(name, args)))
+        Ok(ExprAst::Call { callee: name, args })
     }
 
     // primary_expr
@@ -230,7 +371,7 @@ impl<'a> Parser<'a> {
                 return Ok(lhs);
             }
 
-            let bin_op = self.lexer.gettok()?.get_op_name_or_panic();
+            let op = self.lexer.gettok()?.get_op_name_or_panic();
             let mut rhs = self.parse_primary_expr()?;
 
             let next_prec = self.get_prec()?;
@@ -238,7 +379,7 @@ impl<'a> Parser<'a> {
                 rhs = self.parse_binop_rhs(tok_prec + 1, rhs)?;
             }
 
-            lhs = ExprAst::Binary(BinaryExprAst::new(bin_op, lhs, rhs));
+            lhs = ExprAst::Binary { op, lhs: Box::from(lhs), rhs: Box::from(rhs) };
         }
     }
 
@@ -310,17 +451,46 @@ impl<'a> Parser<'a> {
     }
 }
 
-fn handle_def(parser: &mut Parser) -> ParseResult<()> {
-    println!("{:?}", parser.parse_definition()?);
+fn write_err(s: &str) {
+    write!(io::stderr(), "{}", s).unwrap();
+}
+
+fn handle_def(parser: &mut Parser, llvm: &mut LLVM) -> ParseResult<()> {
+    let fn_ast = parser.parse_definition()?;
+    let fn_ir = fn_ast.codegen(llvm)?;
+    if !fn_ir.is_null() {
+        write_err("Read function definition:");
+        unsafe {
+            llvm_sys::core::LLVMDumpValue(fn_ir);
+        }
+        write_err("\n");
+    }
     Ok(())
 }
 
-fn handle_extern(parser: &mut Parser) -> ParseResult<()> {
-    println!("{:?}", parser.parse_extern()?);
+fn handle_extern(parser: &mut Parser, llvm: &mut LLVM) -> ParseResult<()> {
+    let proto_ast = parser.parse_extern()?;
+    let fn_ir = proto_ast.codegen(llvm);
+    if !fn_ir.is_null() {
+        write_err("Read extern:");
+        unsafe {
+            llvm_sys::core::LLVMDumpValue(fn_ir);
+        }
+        write_err("\n");
+    }
     Ok(())
 }
 
-fn handle_toplevel_expr(parser: &mut Parser) -> ParseResult<()> {
+fn handle_toplevel_expr(parser: &mut Parser, llvm: &mut LLVM) -> ParseResult<()> {
+    let fn_ast = parser.parse_toplevel_expr()?;
+    let fn_ir = fn_ast.codegen(llvm)?;
+    if !fn_ir.is_null() {
+        write_err("Read top-level expression:");
+        unsafe {
+            llvm_sys::core::LLVMDumpValue(fn_ir);
+        }
+        write_err("\n");
+    }
     println!("{:?}", parser.parse_toplevel_expr()?);
     Ok(())
 }
@@ -331,13 +501,14 @@ fn main() -> Result<(), Cow<'static, str>> {
     stdin.lock().read_to_string(&mut src).map_err(|_| Cow::from("cannot read stdin"))?;
 
     let mut parser = Parser::new(&src);
+    let mut llvm = LLVM::new();
     loop {
         match parser.lexer.peektok()? {
             Token::Eof => break,
             Token::Op(';') => { parser.lexer.gettok()?; },
-            Token::Def => handle_def(&mut parser)?,
-            Token::Extern => handle_extern(&mut parser)?,
-            _ => handle_toplevel_expr(&mut parser)?,
+            Token::Def => handle_def(&mut parser, &mut llvm)?,
+            Token::Extern => handle_extern(&mut parser, &mut llvm)?,
+            _ => handle_toplevel_expr(&mut parser, &mut llvm)?,
         }
     }
 
